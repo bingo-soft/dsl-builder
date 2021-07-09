08c797cf73cad64f45ee4e5f271f9927dd1db049
Bingo-Soft <bingosoft@bk.ru>
2021-07-09 14:13:21 +0300

    fix: fix backend factory

--- src/backend/BackendFactory.ts
+++ src/backend/BackendFactory.ts
@@ -1,9 +1,13 @@
 import AbstractBackend from './AbstractBackend'
+import TinyInterpreter from '../language/Tiny/backend/TinyInterpreter'
 
 export default class BackendFactory
 {
-  static createBackend(language: string): AbstractBackend | null
+  static createBackend(lang: string): AbstractBackend | null
   {
+    if (lang == "tiny") {
+      return new TinyInterpreter()
+    }
     return null
   }
 }
